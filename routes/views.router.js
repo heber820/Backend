fbd416fda8a99b331e8550ba213f36e834edd9bc
heber820 <heber_820@hotmail.com>
2023-03-02 22:53:21 -0300

    Login por Formulario

--- routes/views.router.js
+++ routes/views.router.js
@@ -5,6 +5,7 @@ import {CartManager} from '../src/dao/mongoManager/cartManager.js'
 import socketServer from "../src/app.js";
 import {productsModel} from '../src/dao/models/products.model.js';
 import { cartsModel } from '../src/dao/models/carts.model.js';
+import {auth, isLogged, isAdmin, isUser} from '../middlewares/auth.middleware.js'
 
 const viewsRouter = Router()
 // const productManager = new ProductManager('./archivos/products.json') 
@@ -27,8 +28,7 @@ viewsRouter.get('/realtimeproducts',async (req,res)=>{
     res.render('realTimeProducts', {products})
 })
 
-
-viewsRouter.get('/products',async(req,res)=>{
+viewsRouter.get('/admin',auth,isAdmin,async(req,res)=>{
     try {
         // /?limit=1&page=1
         const {limit=10, page=1, category} = req.query //default 10 y 1
@@ -39,7 +39,24 @@ viewsRouter.get('/products',async(req,res)=>{
           products = await productsModel.find({category}).limit(limit).skip(page-1).lean()
         }
         console.log(products)
-          res.render('products', {products})
+          res.render('admin', {products, email:req.session.email, first_name:req.session.first_name, last_name:req.session.last_name, role: req.session.role})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+viewsRouter.get('/products',auth, async(req,res)=>{
+    try {
+        // /?limit=1&page=1
+        const {limit=10, page=1, category} = req.query //default 10 y 1
+        let products 
+        if(!category){
+          products = await productsModel.find().limit(limit).skip(page-1).lean()
+        }else{
+          products = await productsModel.find({category}).limit(limit).skip(page-1).lean()
+        }
+        console.log(products)
+          res.render('products', {products, email:req.session.email, first_name:req.session.first_name, last_name:req.session.last_name, role: req.session.role})
     } catch (error) {
         console.log(error)
     }
@@ -56,6 +73,29 @@ viewsRouter.get('/carts/:cartId', async(req,res) => {
 });
 
 
+viewsRouter.get('/signup',isLogged, (req, res)=>{
+  res.render('signup')
+})
+
+viewsRouter.get('/errorSignup', (req, res)=>{
+  res.render('errorSignup')
+})
+
+viewsRouter.get('/login',isLogged, (req, res)=>{
+  res.render('login')
+})
+
+
+viewsRouter.get('/errorLogin', (req, res)=>{
+  res.render('errorLogin')
+})
+
+viewsRouter.get('/admin', (req, res)=>{
+  res.render('admin')
+})
+
+
+
 //post
 
 viewsRouter.post('/realtimeproducts', async(req, res)=>{
@@ -69,6 +109,5 @@ viewsRouter.post('/realtimeproducts', async(req, res)=>{
     return error
   }
 })
-
 
 export default viewsRouter
