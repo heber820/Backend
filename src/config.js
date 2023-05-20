0673e5ba0a7c47f3f43dc826574c2f74ecd68195
heber820 <heber_820@hotmail.com>
2023-05-19 21:12:08 -0300

    desafioentregable

--- src/config.js
+++ src/config.js
@@ -10,5 +10,6 @@ export default {
     gmail_password:process.env.GMAIL_PASSWORD,
     twilio_phone_number:process.env.TWILIO_PHONE_NUMBER,
     twilio_sid:process.env.TWILIO_SID,
-    twilio_auth_token:process.env.TWILIO_AUTH_TOKEN
+    twilio_auth_token:process.env.TWILIO_AUTH_TOKEN,
+    jwt_key: process.env.JWT_KEY
 }
